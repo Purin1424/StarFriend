2d3c03abb56f7b018e2d6f8a421dab13d306428d
natarika <natarika2555@gmail.com>
2019-02-23 14:45:50 +0700

    Arrow Navigator

--- app/src/main/java/com/starfriend/starfriend/MainFragment.java
+++ app/src/main/java/com/starfriend/starfriend/MainFragment.java
@@ -1,11 +1,15 @@
 package com.starfriend.starfriend;
 
 
+import android.arch.lifecycle.ReportFragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +22,22 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Register Controller
+
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  ReportFragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
+            }
+        });
+
+    }   // Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +46,4 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+}   //Main Class
